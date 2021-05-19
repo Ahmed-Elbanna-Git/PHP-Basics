d0c15e88fb9f37a7119229205c223e9d0b1c35c5
Ahmed-Elbanna108 <ahmed_elbana@aol.com>
2021-05-19 02:02:30 +0200

    here all output syntax

---
+++ OutputSyantax.php
@@ -0,0 +1,29 @@
+<?php
+$var='ahmed';
+//here we add first code to print hello
+//echo "start";
+//print "hello";
+
+//here we add first code to print hello with ()
+//echo ("start");
+//print ("hello");
+
+//here we add first code to print hello print not support multi arguments
+//echo "hello" ,"ahmed";
+//print "hello" ;
+
+//here we add first code to print hello with html tages
+//echo ("<h1>start</h1>");
+//print ("<h1>start</h1>");
+?>
+//for short tag
+<h1>start <?php echo $var ?></h1>
+print ("<h1>start</h1>");
+
+
+
+
+
+
+
+
