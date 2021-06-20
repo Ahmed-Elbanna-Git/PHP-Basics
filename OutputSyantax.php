836e902c2103bc7f70ef720711d1646106f6ab61
ahmed elbanna <ahmed_elbana@aol.com>
2021-06-20 04:48:26 +0200

    here all outeeeeeeeeeeeeeput syntax

--- OutputSyantax.php
+++ OutputSyantax.php
@@ -12,18 +12,28 @@ $var='ahmed';
 //echo "hello" ,"ahmed";
 //print "hello" ;
 
-//here we add first code to print hello with html tages
+//here we add first code to print hello with html tage
 //echo ("<h1>start</h1>");
 //print ("<h1>start</h1>");
-?>
-//for short tag
-<h1>start <?php echo $var ?></h1>
-print ("<h1>start</h1>");
+
+// print  we use boolean to improve that value is true from  lang construct and not a real  functions print
+//echo print 'hello';
+
+//when echo is lang construct  when can't overload it
 
 
+//echo "hello".$var. "\nahmed";
 
+//what faster '' or "" all statement "" is faster except  when you had value to parse $value
 
+//quiz of output topics what the difference between
 
+echo 'hello' .$var;
 
+echo "hello $var";
 
+echo "hello" ,$var;
 
+echo 'hello' ,$var;
+
+echo "hello" .$var;
