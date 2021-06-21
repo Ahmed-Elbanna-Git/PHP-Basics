e6be7fa7ac9118a37f74f3a51dd33b8657c8ccf8
ahmed elbanna <ahmed_elbana@aol.com>
2021-06-21 23:05:01 +0200

    here all outeeeeeeeeeeeeeput syntax

--- OutputSyantax.php
+++ OutputSyantax.php
@@ -37,3 +37,6 @@ echo "hello" ,$var;
 echo 'hello' ,$var;
 
 echo "hello" .$var;
+
+
+
---
+++ Variable Handling functions.php
@@ -0,0 +1,43 @@
+<?php
+//$var = 'Ahmed';
+//var_dump($var);
+
+//$var = 'Ahmed';
+//var_export($var,true);
+
+
+//$var = 'Ahmed';
+//print_r($var);
+
+
+//$var = 'Ahmed';
+//echo 'my name is '.$var;
+//or
+//echo "my name is $var";
+
+//$user_update =false;
+//if($user_update === True){
+//  echo 'yes';
+//}else{
+//  echo 'No';
+//}
+
+//  $num_int = 3;
+//echo $num_int;
+
+//$num_decimal = 3.215;
+//echo $num_decimal ;
+
+//$array = array('1',2,true,2.536);
+//var_dump($array);
+
+
+//$name ='moahamed';
+//$$name = 'ahmed';
+
+//echo $moahamed;
+///complex variable
+${'user'} ='mohamed';
+echo ${'user'};
+
+
